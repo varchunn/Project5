86db560e9811fd690ae24b7cb31b9aec7cf4a8b8
varchunn <varvaram186@gmail.com>
2024-10-14 13:53:17 +0300

    Update MyString.cpp

--- Project5/MyString.cpp
+++ Project5/MyString.cpp
@@ -1,52 +1,55 @@
-﻿#include <iostream>   
+#include <iostream>
 #include "MyString.h" 
 using namespace std;
 
-int MyString::GetCount()//ñòàòè÷íà ïàì'ÿòü
+int MyString::GetCount()
 {
     return count;
 }
-MyString::MyString()
+
+MyString::MyString() 
 {
     str = nullptr;
     length = 0;
     count++;
 }
-MyString::MyString(const char* string)
+
+MyString::MyString(const char* string) 
 {
     str = new char[strlen(string) + 1];
     strcpy_s(str, strlen(string) + 1, string);
-    length = strlen(string + 1);
+    length = strlen(string);
     count++;
 }
-MyString::MyString(const MyString& string)//êîíñòðóêòîð êîï³þâàííÿ
+
+MyString::MyString(const MyString& string) 
 {
     str = new char[strlen(string.str) + 1];
     strcpy_s(str, strlen(string.str) + 1, string.str);
-    length = strlen(string.str + 1);
+    length = strlen(string.str);
 }
-MyString::MyString(MyString&& obj)
+
+MyString::MyString(MyString& obj) 
 {
     str = obj.str;
     length = obj.length;
     obj.str = nullptr;
     obj.length = 0;
-
     cout << "Move constr" << endl;
 }
 
-MyString::MyString(initializer_list<char> arr)
+MyString::MyString(const char* arr) 
 {
     cout << "initializer list" << endl;
-    cout << arr.size() << endl;
-    length = arr.size();
+    length = strlen(arr);
     str = new char[length + 1];
-    for (auto x = arr.begin(); x != arr.end(); x++)
+
+    for (int i = 0; i < length; i++) 
     {
-        *str = *x;
-        str++;
+        str[i] = arr[i]; 
     }
-    str -= length;
+
+    str[length] = '\0';
 }
 
 void MyString::Input()
@@ -54,95 +57,111 @@ void MyString::Input()
     char string[225];
     cout << "Enter text: ";
     cin.getline(string, 225);
-    str = new char[strlen(string) + 1];
-    strcpy_s(str, strlen(string) + 1, string);
-}
-void MyString::Output()
-{
-    cout << str;
+    SetStr(string);  
 }
 
-void MyString::MyStrcpy(MyString& obj)//êîïèðîâàíèå ñòðîê   
+void MyString::Output() const
 {
-    str = new char[strlen(obj.str) + 1];
-    strcpy_s(str, strlen(obj.str) + 1, obj.str);
-    length = obj.length;
+    cout << GetStr(); 
 }
-bool MyString::MyStrStr(const char* string)//ïîèñê ïîäñòðîêè â ñòðîêå   
+
+const char* MyString::GetStr() const 
+{
+    return str;
+}
+
+void MyString::SetStr(const char* newStr) 
+{
+    if (str != nullptr) 
+    {
+        delete[] str;
+    }
+    str = new char[strlen(newStr) + 1];
+    strcpy_s(str, strlen(newStr) + 1, newStr);
+    length = strlen(newStr);
+}
+
+ostream& operator<<(ostream& out, const MyString& obj)
+{
+    out << obj.GetStr(); 
+    return out;
+}
+
+istream& operator>>(istream& in, MyString& obj)
+{
+    char buffer[256];
+    in.getline(buffer, 256);
+    obj.SetStr(buffer);  
+    return in;
+}
+
+void MyString::MyStrcpy(MyString& obj) 
+{
+    SetStr(obj.GetStr()); 
+}
+
+bool MyString::MyStrStr(const char* string) 
 {
     for (int i = 0; i < strlen(string); i++)
-    {
-        for (int j = 0; j < strlen(str); j++)
         {
-            if (string[i] == str[j])
+        for (int j = 0; j < strlen(str); j++)
             {
-                return true;
+            if (string[i] == str[j]) {
                 cout << "Found!";
+                return true;
                 break;
-            }
-            else
-            {
-                return false;
+            } else {
                 cout << "Not found";
+                return false;
             }
-
         }
     }
 }
-int MyString::MyChr(char c)//ïîèñê ñèìâîëà â ñòðîêå(èíäåêñ íàéäåííîãî ñèìâîëà, ëèáî -1)   
+
+int MyString::MyChr(char c)
 {
     for (int i = 0; i < strlen(str); i++)
-    {
+        {
         if (str[i] == c)
         {
             return i + 1;
         }
     }
+    return -1;
 }
-int MyString::MyStrLen()//âîçâðàùàåò äëèíó ñòðîêè   
+
+int MyString::MyStrLen() const
 {
     return strlen(str);
 }
-void MyString::MyStrCat(MyString& obj)//îáúåäèíåíèå ñòðîê   
-{
-    str = new char[strlen(str) + strlen(obj.str) + 1];
-    strcpy_s(str, strlen(obj.str) + 1, obj.str);
-    for (int i = 0; i < strlen(obj.str); i++)
-    {
-        str[i] = obj.str[i];
-    }
-    for (int i = 0; i < strlen(str); i++)
-    {
-        str[i] = obj.str[i];
-    }
-    str[strlen(obj.str) + strlen(str)] = '\0';
-}
-void MyString::MyDelChr(char c)// óäàëÿåò óêàçàííûé ñèìâîë    
-{
 
+void MyString::MyStrCat(MyString& obj)
+{
+    str = new char[strlen(str) + strlen(obj.GetStr()) + 1];
+    strcat_s(str, strlen(str) + strlen(obj.GetStr()) + 1, obj.GetStr());
+}
+
+void MyString::MyDelChr(char c) 
+{
     for (int i = 0; i < strlen(str); i++)
-    {
-        if (str[i] == c)
+        {
+        if (str[i] == c) 
         {
             length--;
             str[i] = str[i + 1];
         }
     }
 }
-int MyString::MyStrCmp(MyString& obj)/*ñðàâíåíèå ñòðîê
-- 1 – ïåðâàÿ ñòðîêà ìåíüøå ÷åì âòîðàÿ
-1 – ïåðâàÿ áîëüøå ÷åì âòîðàÿ
-0 – ñòðîêè ðàâíû!*/
+
+int MyString::MyStrCmp(MyString& obj)
 {
-    if (strlen(str) == strlen(obj.str))
+    if (strlen(str) == strlen(obj.GetStr()))
     {
         return 0;
-    }
-    if (strlen(str) > strlen(obj.str))
+    } else if (strlen(str) > strlen(obj.GetStr())) 
     {
-        return  -1;
-    }
-    if (strlen(str) < strlen(obj.str))
+        return -1;
+    } else 
     {
         return 1;
     }
@@ -150,116 +169,80 @@ int MyString::MyStrCmp(MyString& obj)/*ñðàâíåíèå ñòðîê
 
 MyString& MyString::operator=(const MyString& obj)
 {
-    if (this == &obj)
+    if (this == &obj) 
     {
         return *this;
     }
-    if (str != nullptr)
-    {
-        delete[] str;
-    }
-    length = obj.length;
-
-    str = new char[strlen(obj.str) + 1];
-    strcpy_s(str, strlen(obj.str) + 1, obj.str);
-
-    return*this;
+    SetStr(obj.GetStr());  
+    return *this;
 }
-MyString& MyString::operator=(MyString&& obj)
-{
-    if (str != nullptr)
-    {
-        delete[] str;
-    }
-    str = obj.str;
-    length = obj.length;
-    obj.str = nullptr;
-    obj.length = 0;
 
+MyString& MyString::operator=(MyString& obj)
+{
+    if (this != &obj)
+    {
+        SetStr(obj.GetStr());
+        length = obj.length;
+        obj.str = nullptr;
+        obj.length = 0;
+    }
     cout << "Move =" << endl;
     return *this;
 }
 
-MyString& MyString::operator+=(const MyString& obj)
+MyString& MyString::operator+=(const MyString& obj) 
 {
-    MyString temp = *this;
-    int* temp1 = new int[length + obj.length];
-    for (int i = 0; i < length; i++)
-    {
-        temp1[i] = str[i];
-    }
-    for (int i = length; i < length + obj.length; i++)
-    {
-        temp1[i] = obj.str[i];
-    }
+    int newLength = length + obj.length;
+    char* tempStr = new char[newLength + 1];
+    strcpy_s(tempStr, length + 1, str);
+    strcat_s(tempStr, newLength + 1, obj.str);
+    
     delete[] str;
-    *str = *temp1;
-    length += obj.length;
-    return temp;
+    str = tempStr;
+    length = newLength;
+    
+    return *this;
 }
 
-char MyString::operator[](int index)
+char MyString::operator[](int index) const
 {
     return str[index];
 }
-void MyString::operator()()
+
+void MyString::operator()() const
 {
     cout << str << endl;
 }
 
 MyString MyString::operator+(char c) 
 {
-    char* newStr = new char[length + 2]; 
-    strcpy_s(newStr, length + 1, str);  
-    newStr[length] = c;                  
-    newStr[length + 1] = '\0';          
+    char* newStr = new char[length + 2];
+    strcpy_s(newStr, length + 1, str);
+    newStr[length] = c;
+    newStr[length + 1] = '\0';
     MyString temp(newStr);
     delete[] newStr;
     return temp;
 }
 
-
-MyString operator+(char c, const MyString& obj)
+MyString MyString::operator+(int num)
 {
-    char* newStr = new char[obj.length + 2]; 
-    newStr[0] = c;                           
-    strcpy_s(newStr + 1, obj.length + 1, obj.str); 
-    MyString temp(newStr);
-    delete[] newStr;
-    return temp;
-}
-
-
-MyString MyString::operator+(int num) 
-{
-    if (num <= 0) return *this; // Нічого не додаємо, якщо число <= 0
-    char* newStr = new char[length + num + 1]; 
-    strcpy_s(newStr, length + 1, str);         
-    for (int i = 0; i < num; ++i) {
-        newStr[length + i] = ' ';              
+    if (num <= 0) return *this;
+    char* newStr = new char[length + num + 1];
+    strcpy_s(newStr, length + 1, str);
+    for (int i = 0; i < num; ++i) 
+    {
+        newStr[length + i] = ' ';
     }
-    newStr[length + num] = '\0';               
+    newStr[length + num] = '\0';
     MyString temp(newStr);
     delete[] newStr;
     return temp;
 }
 
-MyString operator+(int num, const MyString& obj)
+MyString::~MyString() 
 {
-    if (num <= 0) return obj; // Нічого не додаємо, якщо число <= 0
-    char* newStr = new char[obj.length + num + 1]; 
-    strcpy_s(newStr, obj.length + 1, obj.str);     
-    for (int i = 0; i < num; ++i) {
-        newStr[obj.length + i] = ' ';              
-    }
-    newStr[obj.length + num] = '\0';               
-    MyString temp(newStr);
-    delete[] newStr;
-    return temp;
-}
-MyString::~MyString()
-{
-    delete[]str;
+    delete[] str;
     length = 0;
     count--;
 }
